ce6ee0549b8660a4582de065921f226b9be5b8f2
蒋伟 <872505975@qq.com>
2016-05-13 17:25:01 +0800

    test

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 app.listen(1338);
 app.get('/',function(req,res){
-  res.send("你好home1338");
+  res.send("你好home1338sdafdsjkfdsf");
 });
 app.get('/test',function(req,res){
   res.send("你好test1338");
